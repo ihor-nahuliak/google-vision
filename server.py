9f590a1ae70e030e9ebba5183743b4e6bfc2f08b
Troy Lamerton <troy.lamerton@gmail.com>
2017-09-23 22:09:30 +0200

    use flask for basic image server

--- server.py
+++ server.py
@@ -1,38 +1,14 @@
-import time
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask, request
+import os
+app = Flask(__name__)
 
-HOST_NAME = 'localhost'
-PORT_NUMBER = 9000
-
-# nothing
-
-class MyHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        for line in self.rfile.read():
-            print(line)
-        self.handle_json(200)
-
-    def handle_json(self, status_code):
-        self.send_response(status_code)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        content = '''
-        <html><head><title>Title goes here.</title></head>
-        </body></html>
-        '''
-        return bytes(content, 'UTF-8')
-
-    def respond(self, opts):
-        response = self.handle_json(opts['status'], self.path)
-        self.wfile.write(response)
-
-if __name__ == '__main__':
-    server_class = HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
+@app.route('/', methods=['GET','POST'])
+def GetNoteText():
+    print(request)
+    if request.method == 'POST':
+        file = request.files['pic']
+        filename = file.filename
+        file.save(os.path.join(os.environ['UPLOAD_FOLDER'], filename))
+        # processImage(filename)            
+    else:
+        return "Y U NO USE POST?"
