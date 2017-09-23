9d56b36b82377a314085cac2ffbc5676bd68e7bb
yacotaco <kamilkurach@yahoo.pl>
2017-09-24 00:47:06 +0200

    Add main.

--- google_api.py
+++ google_api.py
@@ -6,43 +6,49 @@ import json
 from processjson import Processor
 import os
 
-def detect_face(face_file, max_results=4):
-    """Uses the Vision API to detect faces in the given file.
-    Args:
-        face_file: A file-like object containing an image with faces.
-    Returns:
-        An array of Face objects with information about the picture.
-    """
-    # [START get_vision_service]
-    client = vision.ImageAnnotatorClient()
-    # [END get_vision_service]
+class Vision:
 
-    content = face_file.read()
-    image = types.Image(content=content)
+    def detect_face(self, face_file, max_results=4):
+        """Uses the Vision API to detect faces in the given file.
+        Args:
+            face_file: A file-like object containing an image with faces.
+        Returns:
+            An array of Face objects with information about the picture.
+        """
+        # [START get_vision_service]
+        client = vision.ImageAnnotatorClient()
+        # [END get_vision_service]
 
-    #request = {'image': {'source': {'image_uri': '{}'.format(url)},},}
+        content = face_file.read()
+        image = types.Image(content=content)
 
-    return client.face_detection(image=image).face_annotations
+        #request = {'image': {'source': {'image_uri': '{}'.format(url)},},}
+
+        return client.face_detection(image=image).face_annotations
 
 
-url='https://farm4.staticflickr.com/3296/2760862365_45e011e538_z.jpg?zz%5Cx3d1'
-max_results=1
-input_filename = '/home/slippy/Dokumenty/face.jpg'
 
-with open(input_filename, 'rb') as image:
-    face = detect_face(image, max_results)
-    s_face = str(face)
-    f = open('test.txt', 'w')
-    f.write(s_face)
-    f.close()
+def main():
+    max_results=1
+    input_filename = '/home/slippy/Dokumenty/face.jpg'
+    v = Vision()
+    with open(input_filename, 'rb') as image:
+        face = v.detect_face(image, max_results)
+        s_face = str(face)
+        f = open('test.txt', 'w')
+        f.write(s_face)
+        f.close()
 
-    if os.path.isfile('test.txt'):
-        file = open('test.txt')
-        p = Processor(file)
-        print(p.get_joy_value())
-        print(p.get_sorrow_value())
-        print(p.get_anger_value())
-        print(p.get_surprise_value())
-        print(p.get_underexposed_value())
-        print(p.get_blurred_value())
-        print(p.get_headwear_value())
+        if os.path.isfile('test.txt'):
+            file = open('test.txt')
+            p = Processor(file)
+            print(p.get_joy_value())
+            print(p.get_sorrow_value())
+            print(p.get_anger_value())
+            print(p.get_surprise_value())
+            print(p.get_underexposed_value())
+            print(p.get_blurred_value())
+            print(p.get_headwear_value())
+
+if __name__ == '__main__':
+    main()
